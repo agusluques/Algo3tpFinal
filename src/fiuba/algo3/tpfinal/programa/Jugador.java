697800852e14b0abee19969651e5ccda27cabb6a
DamiCassinotti <damiancassinotti.33@gmail.com>
2015-05-29 17:13:59 -0300

    Agrego la clase Jugador (con sus tests) y dos excepciones

---
+++ src/fiuba/algo3/tpfinal/programa/Jugador.java
@@ -0,0 +1,52 @@
+package fiuba.algo3.tpfinal.programa;
+
+import fiuba.algo3.tpfinal.excepciones.GasInsuficienteException;
+import fiuba.algo3.tpfinal.excepciones.MineralInsuficienteException;
+
+public class Jugador {
+
+	private String nombre;
+	private int cantidadDeGas;
+	private int cantidadDeMineral;
+
+	public Jugador(String nombre) {
+		this.nombre = nombre;
+		this.cantidadDeGas = 0;
+		this.cantidadDeMineral = 0;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getCantidadDeGas() {
+		return this.cantidadDeGas;
+	}
+	
+	public int getCantidadDeMineral() {
+		return this.cantidadDeMineral;
+	}
+
+	public void agregarMineral(int cantidad) {
+		this.cantidadDeMineral += cantidad;
+	}
+	
+	public void agregarGas(int cantidad) {
+		this.cantidadDeGas += cantidad;
+	}
+
+	public void gastarMineral(int cantidad) {
+		if (this.cantidadDeMineral < cantidad) {
+			throw new MineralInsuficienteException();
+		}
+		this.cantidadDeMineral -= cantidad;		
+	}
+	
+	public void gastarGas(int cantidad) {
+		if (this.cantidadDeGas < cantidad) {
+			throw new GasInsuficienteException();
+		}
+		this.cantidadDeGas -= cantidad;		
+	}
+
+}
---
+++ test/fiuba/algo3/tpfinal/programa/JugadorTest.java
@@ -0,0 +1,85 @@
+package fiuba.algo3.tpfinal.programa;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fiuba.algo3.tpfinal.excepciones.GasInsuficienteException;
+import fiuba.algo3.tpfinal.excepciones.MineralInsuficienteException;
+
+public class JugadorTest {
+	
+	private Jugador jugador;
+
+	@Before
+	public void arrange(){
+		this.jugador = new Jugador("Damian");
+	}
+	
+	@Test
+	public void elJugadorComienzaConUnNombre() {		
+		Assert.assertEquals("Damian", jugador.getNombre());
+	}
+	
+	@Test
+	public void elJugadorComienzaConCeroGas() {
+		Assert.assertEquals(0, jugador.getCantidadDeGas());
+	}
+	
+	@Test
+	public void elJugadorComienzaConCeroMineral() {
+		Assert.assertEquals(0, jugador.getCantidadDeMineral());
+	}
+	
+	@Test
+	public void siElJugadorSuma20DeMineralTiene20() {
+		jugador.agregarMineral(20);
+		Assert.assertEquals(20, jugador.getCantidadDeMineral());
+	}
+	
+	@Test
+	public void siElJugadorSuma20DeGasTiene20() {
+		jugador.agregarGas(20);
+		Assert.assertEquals(20, jugador.getCantidadDeGas());
+	}
+	
+	@Test
+	public void siElJugadorSuma20DeMineralYGasta10Tiene10() {
+		jugador.agregarMineral(20);
+		jugador.gastarMineral(10);
+		Assert.assertEquals(10, jugador.getCantidadDeMineral());
+	}
+	
+	@Test
+	public void siElJugadorSuma20DeGasYGasta10Tiene10() {
+		jugador.agregarGas(20);
+		jugador.gastarGas(10);
+		Assert.assertEquals(10, jugador.getCantidadDeGas());
+	}
+	
+	@Test
+	public void siElJugadorGastaMasGasDelQueTieneSeLanzaExcepcion() {
+		jugador.agregarGas(10);
+		try {
+			jugador.gastarGas(30);
+			Assert.assertTrue(false);
+		} catch (GasInsuficienteException e){
+			Assert.assertTrue(true);
+		}
+
+	}
+	
+	@Test
+	public void siElJugadorGastaMasMineralDelQueTieneSeLanzaExcepcion() {
+		jugador.agregarMineral(10);
+		try {
+			jugador.gastarMineral(30);
+			Assert.assertTrue(false);
+		} catch (MineralInsuficienteException e){
+			Assert.assertTrue(true);
+		}
+
+	}
+
+}
