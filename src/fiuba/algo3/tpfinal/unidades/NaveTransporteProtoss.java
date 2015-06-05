b18ead38f4071c776caaf09e996718b497f30a8d
Agus Luques <agus.luques@hotmail.com>
2015-06-04 21:45:38 -0300

    Clase NaveTransporteProtoss.

---
+++ src/fiuba/algo3/tpfinal/unidades/NaveTransporteProtoss.java
@@ -0,0 +1,17 @@
+package fiuba.algo3.tpfinal.unidades;
+
+import fiuba.algo3.tpfinal.programa.Danio;
+
+public class NaveTransporteProtoss extends UnidadesProtoss {
+
+	public NaveTransporteProtoss(){
+		this.vida.inicializarVida(80);
+		this.escudo.inicializarEscudo(60);
+		this.miDanio = new Danio(0,0);
+	}
+	
+	public void atacado(Danio danio){
+		this.escudo.bajarEscudo(danio.getDanioAire(), this.vida);
+
+	}
+}
---
+++ test/fiuba/algo3/tpfinal/unidades/NaveTransporteProtossTest.java
@@ -0,0 +1,47 @@
+package fiuba.algo3.tpfinal.unidades;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import fiuba.algo3.tpfinal.programa.Danio;
+
+public class NaveTransporteProtossTest {
+
+	@Test
+	public void siSeCreaUnaNaveTransporteProtossDebeTener80DeVidaY60DeEscudo(){
+		NaveTransporteProtoss nave = new NaveTransporteProtoss();
+		Assert.assertTrue(nave.getVida() == 80);
+		Assert.assertTrue(nave.getEscudo() == 60);
+		
+	}
+	
+	@Test
+	public void siAUnaNaveTransporteProtossLePegan100DebeTener40DeVida(){
+		NaveTransporteProtoss nave = new NaveTransporteProtoss();
+		Danio danio = new Danio(100,0);
+		nave.atacado(danio);
+		Assert.assertTrue(nave.getVida() == 40);
+	}
+	
+	@Test
+	public void siAUnaNaveTransporteProtossLePegan800DebeTener0(){
+		NaveTransporteProtoss nave = new NaveTransporteProtoss();
+		Danio danio = new Danio(800,0);
+		nave.atacado(danio);
+		Assert.assertTrue(nave.getVida() == 0);
+	}
+	
+	@Test
+	public void siUnDragonAtacaAUnaNaveTransporteProtossAlSegundoLeBajaElEscudoA40YNoBajaVida() {
+		
+		Dragon unDragon = new Dragon();
+		NaveTransporteProtoss nave = new NaveTransporteProtoss();
+		unDragon.atacar(nave);
+		Assert.assertTrue(nave.getVida() == 80);
+		Assert.assertTrue(nave.getEscudo() == 40);
+		
+	}
+	
+
+}
