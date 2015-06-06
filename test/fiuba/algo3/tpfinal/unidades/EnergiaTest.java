0ba146515965a52c42ee15f4f9d90a532632fd7d
DamiCassinotti <damiancassinotti.33@gmail.com>
2015-06-06 20:36:56 -0300

    Subo la clase energía y sus tests

---
+++ test/fiuba/algo3/tpfinal/unidades/EnergiaTest.java
@@ -0,0 +1,71 @@
+package fiuba.algo3.tpfinal.unidades;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import fiuba.algo3.tpfinal.excepciones.EnergiaInsuficiente;
+
+public class EnergiaTest {
+	
+	@Test
+	public void laEnergiaSeIniciaConCantidadInicial() {
+		Energia energia = new Energia(50);
+		
+		Assert.assertEquals(50, energia.getEnergia());
+	}
+	
+	@Test
+	public void siInicioCon50YGasto10Tengo40() {
+		Energia energia = new Energia(50);
+		
+		energia.gastarEnergia(10);
+		
+		Assert.assertEquals(40, energia.getEnergia());
+	}
+	
+	@Test(expected = EnergiaInsuficiente.class)
+	public void siQuieroGastarMasEnergiaDeLaQueTengoLanzaExcepcion() {
+		Energia energia = new Energia(50);
+		
+		energia.gastarEnergia(60);
+	}
+	
+	@Test
+	public void siGastoMasEnergiaDeLaQueTengoLanzaExcepcionYNoSeModificaLaCantidad() {
+		Energia energia = new Energia(50);
+		try {			
+			energia.gastarEnergia(60);
+		} catch (EnergiaInsuficiente e) {
+			Assert.assertEquals(50, energia.getEnergia());
+		}
+	}
+	
+	@Test
+	public void siInicioCon50YAumento5Tengo55() {
+		Energia energia = new Energia(50);
+		
+		energia.aumentarEnergia(5);
+		
+		Assert.assertEquals(55, energia.getEnergia());
+	}
+	
+	@Test
+	public void siInicioCon50YAumento150Tengo200() {
+		Energia energia = new Energia(50);
+		
+		energia.aumentarEnergia(150);
+		
+		Assert.assertEquals(200, energia.getEnergia());
+	}
+	
+	@Test
+	public void siInicioCon50YAumento151Tengo200() {
+		Energia energia = new Energia(50);
+		
+		energia.aumentarEnergia(151);
+		
+		Assert.assertEquals(200, energia.getEnergia());
+	}
+
+}
