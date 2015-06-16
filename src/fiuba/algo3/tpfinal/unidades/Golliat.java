f56e7f3b65d6e0b43bb07135f28356706faaff56
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2015-06-15 22:45:55 -0300

    Update Golliat.java
    Comentarios en Golliat

--- src/fiuba/algo3/tpfinal/unidades/Golliat.java
+++ src/fiuba/algo3/tpfinal/unidades/Golliat.java
@@ -9,12 +9,15 @@ public class Golliat extends UnidadTerran {
 	public Golliat() {
 		this.vida.inicializarVida(125);
 		this.miDanio = new Danio(10, 12);
+		
+		//TODO ¿Qué es RANGO ? ¿RANGO DE VISION, RANGO DE ATAQUE ?...
 		this.rango = new Rango(6, 5);
 		this.tiempoDeConstruccion = 6;
 		this.suministro = 2;
 		this.costo = new Costo(100, 50);
 		this.transporte = 2;
-
+		
+		//TODO SACAR !
 		// se inicializa en (0,0) solo para los tests
 		this.posicion = new Coordenada(0, 0);
 	}
