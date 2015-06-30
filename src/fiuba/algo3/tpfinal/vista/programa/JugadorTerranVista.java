2ad3a3762f31ea4d28002ae2f422c9eb7552c1b1
DamiCassinotti <damiancassinotti.33@gmail.com>
2015-06-30 17:31:16 -0300

    Subo la construccion de todos los edificios Terran

--- src/fiuba/algo3/tpfinal/vista/programa/JugadorTerranVista.java
+++ src/fiuba/algo3/tpfinal/vista/programa/JugadorTerranVista.java
@@ -2,7 +2,12 @@ package fiuba.algo3.tpfinal.vista.programa;
 
 import javax.swing.JButton;
 
+import fiuba.algo3.tpfinal.construcciones.Barraca;
 import fiuba.algo3.tpfinal.construcciones.CentroDeMineral;
+import fiuba.algo3.tpfinal.construcciones.DepositoSuministro;
+import fiuba.algo3.tpfinal.construcciones.Fabrica;
+import fiuba.algo3.tpfinal.construcciones.PuertoEstelarTerran;
+import fiuba.algo3.tpfinal.construcciones.Refineria;
 import fiuba.algo3.tpfinal.programa.JugadorTerran;
 import fiuba.algo3.tpfinal.vista.ControladorConstruccionTerran;
 
@@ -15,6 +20,36 @@ public class JugadorTerranVista extends JugadorVista {
 		JButton botonCrearCentroMineral = new JButton("Construir centro de mineral");
 		botonCrearCentroMineral.addActionListener(controladorCentroMineral);
 		miPanel.add(botonCrearCentroMineral);
+		
+		ControladorConstruccionTerran controladorBarraca = new ControladorConstruccionTerran((JugadorTerran) miJugador, Barraca.class);
+		controladorBarraca.setVentanaMapa(ventanaMapa);
+		JButton botonCrearBarraca = new JButton("Construir Barraca");
+		botonCrearBarraca.addActionListener(controladorBarraca);
+		miPanel.add(botonCrearBarraca);
+		
+		ControladorConstruccionTerran controladorDepositoSuministro = new ControladorConstruccionTerran((JugadorTerran) miJugador, DepositoSuministro.class);
+		controladorDepositoSuministro.setVentanaMapa(ventanaMapa);
+		JButton botonCrearDepositoSuministro = new JButton("Construir DepositoSuministro");
+		botonCrearDepositoSuministro.addActionListener(controladorDepositoSuministro);
+		miPanel.add(botonCrearDepositoSuministro);
+		
+		ControladorConstruccionTerran controladorRefineria = new ControladorConstruccionTerran((JugadorTerran) miJugador, Refineria.class);
+		controladorRefineria.setVentanaMapa(ventanaMapa);
+		JButton botonCrearRefineria = new JButton("Construir Refineria");
+		botonCrearRefineria.addActionListener(controladorRefineria);
+		miPanel.add(botonCrearRefineria);
+		
+		ControladorConstruccionTerran controladorFabrica = new ControladorConstruccionTerran((JugadorTerran) miJugador, Fabrica.class);
+		controladorFabrica.setVentanaMapa(ventanaMapa);
+		JButton botonCrearFabrica = new JButton("Construir Fabrica");
+		botonCrearFabrica.addActionListener(controladorFabrica);
+		miPanel.add(botonCrearFabrica);
+		
+		ControladorConstruccionTerran controladorPuertoEstelar = new ControladorConstruccionTerran((JugadorTerran) miJugador, PuertoEstelarTerran.class);
+		controladorPuertoEstelar.setVentanaMapa(ventanaMapa);
+		JButton botonCrearPuertoEstelar = new JButton("Construir PuertoEstelar");
+		botonCrearPuertoEstelar.addActionListener(controladorPuertoEstelar);
+		miPanel.add(botonCrearPuertoEstelar);
 		
 		super.crearControladores();
 	}
